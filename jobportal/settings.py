aaf54f8d53c0a8623bb1204ca458471fc6ca431c
rayrohit999 <rohitkumarara2004@gmail.com>
2025-02-10 22:15:43 +0530

    Till assigment 1 completed

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-csq6*!^y&!=g+y!2rv%yr3v)scw69+tl&%6o^7(aj)+(vn2wt8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
