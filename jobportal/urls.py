107574bd8e6cf709b82f2b842910bad3aa788c9b
rayrohit999 <rohitkumarara2004@gmail.com>
2025-02-10 20:13:44 +0530

    Basic setup complete

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('home.urls')),
 ]
+from django.shortcuts import render
+
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
+
+handler404 = 'jobportal.urls.custom_404_view'
